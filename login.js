bf01390c57b6ed4023776b62977bd7abcbd4385e
NkadimengKgothatso <2840850@Students.wits.ac.za>
2026-03-25 14:39:35 +0200

    made shanges to the animations and appearences

--- login.js
+++ login.js
@@ -1,21 +1,18 @@
+// form-toggle.js
+
+// Wait until DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById('container');
+    const registerSwitch = document.getElementById('registerSwitch');
+    const loginSwitch = document.getElementById('loginSwitch');
 
-const container = document.getElementById('container');
-
-document.getElementById('registerSwitch').addEventListener('click', () => {
-    container.classList.add('right-panel-active');
-});
-
-document.getElementById('loginSwitch').addEventListener('click', () => {
-    container.classList.remove('right-panel-active');
-});
+    // When "Sign Up" button in overlay is clicked
     registerSwitch.addEventListener('click', () => {
-        container.classList.add("active");
+        container.classList.add('active');
     });
 
+    // When "Sign In" button in overlay is clicked
     loginSwitch.addEventListener('click', () => {
-        container.classList.remove("active");
+        container.classList.remove('active');
     });
-
 });
-
